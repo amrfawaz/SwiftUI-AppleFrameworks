0867ce56f232a0ad05bac283499d107418693be1
Amr Fawaz <amr.abdelhakim.fawaz+96@gmail.com>
2023-09-19 02:27:14 +0300

    Initial Commit

---
+++ SwiftUI-AppleFrameworks/SwiftUI_AppleFrameworksApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_AppleFrameworksApp.swift
+//  SwiftUI-AppleFrameworks
+//
+//  Created by Amr Fawaz on 19/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_AppleFrameworksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
